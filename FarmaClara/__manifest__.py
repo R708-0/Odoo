4b9fffe760515281a63da0871ead419585827e4d
Diego Melgar Parada <47795330+R708-0@users.noreply.github.com>
2025-05-25 03:43:14 -0400

    version 1.0.0

--- FarmaClara/__manifest__.py
+++ FarmaClara/__manifest__.py
@@ -3,6 +3,7 @@
     "author": "Diego Melgar Parada",
     "description": "Sistema de farmacias",
     "version": "1.0.0",
+    "depends":['point_of_sale'],
     "installable": True,
     "application": True,
 }
---
+++ FarmaClara/models/farmaclara_ventas.py
@@ -0,0 +1,4 @@
+from odoo import models, fields
+
+class Ventas(models.Moldel):
+    _inherit="pos.config"
