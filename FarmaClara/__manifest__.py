5194f5033c931ee409266531970e34224d0d0bb4
Diego Melgar Parada <47795330+R708-0@users.noreply.github.com>
2025-05-25 03:22:27 -0400

    version 1

--- FarmaClara/__manifest__.py
+++ FarmaClara/__manifest__.py
@@ -2,5 +2,7 @@
     "name": "FarmaClara",
     "author": "Diego Melgar Parada",
     "description": "Sistema de farmacias",
-
+    "version": "1.0.0",
+    "installable": True,
+    "application": True,
 }
--- README.md
+++
@@ -1,2 +0,0 @@
-# Odoo
-aplicaciones de odoo
