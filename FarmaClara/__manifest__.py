d12da22cfee201b6e82f11c836df2d799f0ce7c9
Diego Melgar Parada <47795330+R708-0@users.noreply.github.com>
2025-05-30 16:13:00 -0400

    a

--- FarmaClara/__manifest__.py
+++ FarmaClara/__manifest__.py
@@ -3,16 +3,16 @@
     "author": "Diego Melgar Parada",
     "description": "Sistema de farmacias",
     "version": "1.0.1",
-    "depends":['point_of_sale','stock'],
+    "depends":['point_of_sale','stock','web'],
     "data":[
         'views/farmaclara_ventas.xml',
         'views/farmaclara_inventario.xml',
     ],
-    # "assets":{
-    #     'web.assets.backend':[
-    #         'FarmaClara/static/src/css/farmaclara_style.css',
-    #     ],
-    # },
+    "assets":{
+        'web.assets_backend': [
+            'FarmaClara/static/src/css/farmaclara_style.css',
+        ],
+    },
     "installable": True,
     "application": True,
 }
