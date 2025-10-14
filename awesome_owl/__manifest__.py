f7eebd34f14c027ed58ee167698ffe966ad1ccc3
Diego Melgar Parada <47795330+R708-0@users.noreply.github.com>
2025-10-14 16:13:29 -0400

    dashboard lptm

---
+++ awesome_owl/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name" : "awesome_owl",
+    "description" : "practica para dashboards",
+    "author" : "Diego Melgar Parada",
+    "depends" : ["web"],
+    "data": [
+    ],
+
+    "installable" : True,
+    "application" : False,
+    "auto_install" : False,
+}
