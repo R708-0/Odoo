21698e7db3a3b6bf2d01346970622662426c9aee
Diego Melgar Parada <47795330+R708-0@users.noreply.github.com>
2025-12-04 11:05:14 -0400

    security 1.4

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,5 +1,6 @@
 import { Component } from "@odoo/owl";
 
+// exporta la clase para que pueda ser usada por otro archivo
 export class Playground extends Component {
     static template = "awesome_owl.Playground";
 }
