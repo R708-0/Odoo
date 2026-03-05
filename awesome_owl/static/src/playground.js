c1ab176210bdc7583ab7082d02a0da5574908bfc
Diego Melgar Parada <47795330+R708-0@users.noreply.github.com>
2026-03-04 22:04:48 -0400

    playground

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,8 +1,16 @@
 /** @odoo-module **/
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 // exporta la clase para que pueda ser usada por otro archivo
 export class Playground extends Component {
-    static template = "awesome_owl.Playground";
-    static props = {};
+    static template = "awesome_owl.Playground"; // debe coincidir con el t-name del template
+    static props = {}; // para evitar warnings se crea un props(arguetos) vacio
+
+    setup (){
+        this.state = useState( {value: 0} );
+    }
+
+    increment(){
+        this.state.value++;
+    }
 }
